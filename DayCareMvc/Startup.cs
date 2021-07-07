029c1a3ddbbeb50b90bff30dba82d76227b622db
Yusuf Khan <yusufddkhan@gmail.com>
2021-07-07 17:27:20 +0200

    Added in login page

--- DayCareMvc/Startup.cs
+++ DayCareMvc/Startup.cs
@@ -27,6 +27,15 @@ namespace DayCareMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +57,8 @@ namespace DayCareMvc
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
