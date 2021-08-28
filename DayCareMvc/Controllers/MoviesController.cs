d4ca977669ce4384b55d6168db656c338bb21bfb
Yusuf Khan <yusufddkhan@gmail.com>
2021-08-28 20:54:11 +0200

    Added in new series and movies

---
+++ DayCareMvc/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using DayCareMvc.Interfaces;
+using DayCareMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace DayCareMvc.Controllers
+{
+    public class MoviesController: Controller
+    {
+        private readonly ILogger<MoviesController> _logger;
+        private IEnumerable<MovieModel> movieModels;
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index([FromServices] IMoviesRepository moviesRepository)
+        {
+            ViewBag.MovieSummaryObject = moviesRepository.GetAll();
+            return View();
+        }
+    }
+}
---
+++ DayCareMvc/Interfaces/ISeriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using DayCareMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayCareMvc.Interfaces
+{
+    public interface ISeriesRepository
+    {
+        IEnumerable<SeriesModel> GetAll();
+        IEnumerable<SeriesModel> GetSummary();
+        SeriesModel Get(int Id);
+    }
+}
